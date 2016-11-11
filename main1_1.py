1b3e143c268ea57ea4627b64c7e27c0849cc6e36
EngInk <59070193@it.kmitl.ac.th>
2016-11-11 14:44:01 +0700

    Add function in use

---
+++ main1_1.py
@@ -0,0 +1,38 @@
+"""
+    Prototype of project
+    Collaborators:Jirawan  Chuapradit     59070027
+                  Thanadon Lertrumporn    59070065
+                  Benjawan simpattanawong 59070093
+                  Isara    Phoeon         59070193
+"""
+import matpoltlib as mat
+import numpy as py
+def graph_1():
+    """
+        This function use to calculate population of people with drugs split it with
+         type of sex then show it on linear graph
+    """
+
+def graph_2():
+    """
+        This function use to calculate population of people with drugs split it with age
+        then show it on pie chart 
+    """
+
+def graph_3():
+    """
+        This function use to calculate population of people with drugs split it with type of drugs
+        then show it on bar graph
+    """
+
+def graph_4():
+    """
+        This function use to calculate population of people with drugs split it with reasons that take drugs
+        then show it on pie chart
+    """
+
+def graph_5():
+    """
+        This function use to calculate population of people with drugs split it with cause admitted
+        then show it on pie chart
+    """
