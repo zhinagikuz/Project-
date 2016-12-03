03cce4412e2d1eb43d623ea8c7a808e3cf5e9094
Thanadon65 <59070065@kmitl.ac.th>
2016-12-03 23:23:51 +0700

    edit ages ver percent

--- garph/ages_test.py
+++ garph/ages_test.py
@@ -3,6 +3,7 @@ import pygal
 gauge = pygal.SolidGauge(inner_radius=0.70)
 gauge.title = "กลุ่มอายุการใช้ยาเสพติด ปี 2555-2558"
 percent_formatter = lambda x: '{:.10g}%'.format(x)
+gauge.value_formatter = percent_formatter
 gauge.add('0-14 ปี', [{'value': 2.63, 'max_value': 100}])
 gauge.add('15-24 ปี', [{'value': 36.47, 'max_value': 100}])
 gauge.add('25-54 ปี', [{'value': 58.02, 'max_value': 100}])
