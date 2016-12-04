d38c6f9e391b95b73e83ae68ac2f71402a6119e3
EngInk <59070193@it.kmitl.ac.th>
2016-12-04 19:50:39 +0700

    แก้แล้ว 555

--- garph/whyusepie.py
+++ garph/whyusepie.py
@@ -13,12 +13,12 @@ for i in why:
 # Plot pie_chart
 pie_chart.title = 'สัดส่วนของสาเหตุการใช้ยาตั้งแต่ปี 2555-2558 คิดเป็นเปอร์เซ็นต์'
 #using data 
-pie_chart.add('ทดลอง', data[0] / data[9])
-pie_chart.add('เพื่อนชวน', data[1] / data[9])
-pie_chart.add('สนุกสนาน', data[2] / data[9])
-pie_chart.add('เจ็บป่วย', data[3] / data[9])
-pie_chart.add('มีปัญหา', data[4] / data[9])
-pie_chart.add('ช่วยงานอาชีพ', data[5] / data[9])
-pie_chart.add('มีอาการอย่างเสพ', data[6] / data[9])
-pie_chart.add('อื่นๆ', (data[7] + data[8]) / data[9])
+pie_chart.add('ทดลอง', data[0])
+pie_chart.add('เพื่อนชวน', data[1])
+pie_chart.add('สนุกสนาน', data[2])
+pie_chart.add('เจ็บป่วย', data[3])
+pie_chart.add('มีปัญหา', data[4])
+pie_chart.add('ช่วยงานอาชีพ', data[5])
+pie_chart.add('มีอาการอย่างเสพ', data[6])
+pie_chart.add('อื่นๆ', (data[7] + data[8])
 pie_chart.render_to_file('whyuse.svg')
