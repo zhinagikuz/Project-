58bc13023090276186a25314a93123d04a9f12a5
pp <59070093@it.kmitl.ac.th>
2016-12-05 14:44:50 +0700

    edit path render graph

--- graph/whyusepie.py
+++ graph/whyusepie.py
@@ -10,12 +10,10 @@ for line in file:
 for i in why:
     data.append(int(i[0]) + int(i[1]) + int(i[2]) + int(i[3]))
 
+pie_chart = pygal.Pie()
 # Plot pie_chart
-<<<<<<< HEAD
-pie_chart.title = 'สัดส่วนของสาเหตุการใช้ยาตั้งแต่ปี 2555-2558 คิดเป็นเปอร์เซ็นต์'
-=======
 pie_chart.title = 'สัดส่วนของสาเหตุการใช้ยาตั้งแต่ปี 2555-2558'
->>>>>>> refs/remotes/origin/jugjig
+
 #using data 
 pie_chart.add('ทดลอง', data[0])
 pie_chart.add('เพื่อนชวน', data[1])
@@ -24,5 +22,5 @@ pie_chart.add('เจ็บป่วย', data[3])
 pie_chart.add('มีปัญหา', data[4])
 pie_chart.add('ช่วยงานอาชีพ', data[5])
 pie_chart.add('มีอาการอย่างเสพ', data[6])
-pie_chart.add('อื่นๆ', (data[7] + data[8])
-pie_chart.render_to_file('whyuse.svg')
+pie_chart.add('อื่นๆ', (data[7] + data[8]))
+pie_chart.render_to_file('img-svg/whyusepie.svg')
